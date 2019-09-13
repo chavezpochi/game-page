7a4dfd57d1166c4d79312b80d725d21f67104dec
chavezpochi <rodrigo.94.chavez@gmail.com>
2019-09-13 02:06:21 -0300

    Listo los mini-juegos

--- scripts.js
+++ scripts.js
@@ -2,25 +2,49 @@
 
 // Obtiene el modal
 var modal = document.getElementById("myModal");
-// Obtiene el boton que abre el modal
-var btn = document.getElementById("myBtn");
 // Obtiene el <span> que cierra el modal
 var span = document.getElementsByClassName("close")[0];
+var iframe = null;
 // Cuando el usuario clickea el boton, abre el modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
 // Cuando el usuario hace click en <span> (x), cierra el modal
 span.onclick = function() {
   modal.style.display = "none";
+  cerrar();
 }
 
 // Cuando el usuario hace click fuera del modal se cierra
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    cerrar();
   }
+}
+
+function jugar(juego){
+
+    modal.style.display = "block";
+    var iframe = crearJuego(juego);
+    var padre = document.getElementById("game_drop");
+    document.getElementById("modal-content").appendChild(iframe);
+}
+
+function cerrar(){
+    var iframe = document.getElementById("game_drop");
+    var padre = iframe.parentNode;
+    padre.removeChild(iframe);
+}
+
+function crearJuego(juego){
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute('id', 'game_drop');
+    iframe.setAttribute('src', juego);
+    iframe.setAttribute('scrolling', 'no');
+    iframe.style.width = "50%";
+    iframe.style.height = "50%";
+    iframe.style.border = "0";
+    iframe.style.overflow = "hidden";
+    return iframe;
 }
 
 /* FIN FUNCIONES Y VARIABLES PARA MODAL */
